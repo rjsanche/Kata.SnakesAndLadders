40b55a375edcb411395e88f2bb269b3cba9e8c09
Raúl Jiménez <rjsanche@gmail.com>
2021-09-10 11:39:38 +0200

    US 1- UAT1 - Refactor

--- SnakesAndLadders/SnakesAndLaddersEngine.cs
+++ SnakesAndLadders/SnakesAndLaddersEngine.cs
@@ -8,9 +8,25 @@ namespace SnakesAndLadders
 {
     public class SnakesAndLaddersEngine
     {
+        #region Fields
+        private const int INIT_POSITION = 1;
+        private int  _currentPosition;
+        #endregion
+
+        #region Constructor
+
+        public SnakesAndLaddersEngine()
+        {
+            _currentPosition = INIT_POSITION;
+        }
+        #endregion
+
+        #region Public Methods
+
         public int GetCurrentPosition()
         {
-            return 1;
+            return _currentPosition;
         }
+        #endregion
     }
 }
